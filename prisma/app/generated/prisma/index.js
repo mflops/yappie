63c5546186f7cf58f120c8d92815d807237b8b5a
Madhur Chawla <madhurchawla10f@gmail.com>
2025-05-24 05:55:54 +0530

    Ran prisma generate

--- prisma/app/generated/prisma/index.js
+++ prisma/app/generated/prisma/index.js
@@ -243,7 +243,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../../.env"
   },
   "relativePath": "../../..",
